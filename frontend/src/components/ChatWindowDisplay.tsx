f8027d91f208cbabc9335976e4651850a3619cb1
seideljonas <jonas.seidel1@rwth-aachen.de>
2025-06-13 09:12:52 +0200

    Minimum size of ChatDisplay

--- frontend/src/components/ChatWindowDisplay.tsx
+++ frontend/src/components/ChatWindowDisplay.tsx
@@ -8,31 +8,28 @@ interface ChatWindowProps {
 function ChatWindowDisplay({messages}: ChatWindowProps) {
 
   return (
-      <>
-      <div className="vstack gap-3 p-3 mx-auto border bg-light w-50 rounded-4">
-        {messages.map(([message, source], index) =>
-          source % 2 === 1 ? (
-            <div className="card rounded-4 align-self-end" key={`${index}-question`}>
-              <div className="card-body">
-                <h6 className="card-title text-primary">You</h6>
-                <ReactMarkdown >{message}</ReactMarkdown>
-                
+      <div className="container border rounded-4 min-vh-100 w-50 bg-light p-3">
+        <div className="vstack gap-3 mx-auto">
+          {messages.map(([message, source], index) =>
+            source % 2 === 1 ? (
+              <div className="card rounded-4 align-self-end" key={`${index}-question`}>
+                <div className="card-body">
+                  <h6 className="card-title text-primary">You</h6>
+                  <ReactMarkdown >{message}</ReactMarkdown>
+                  
+                </div>
               </div>
-            </div>
-          ) : (
-            
-            <div className="card rounded-4 align-self-start" key={`${index}-answer`}>
-              <div className="card-body">
-                <h6 className="card-title text-success">Bot</h6>
-                <ReactMarkdown >{message}</ReactMarkdown>
-                
+            ) : (
+              
+              <div className="card rounded-4 align-self-start" key={`${index}-answer`}>
+                <div className="card-body">
+                  <h6 className="card-title text-success">Bot</h6>
+                  <ReactMarkdown >{message}</ReactMarkdown>
+                  
+                </div>
               </div>
-            </div>
-          )
-        )}
-      </div>
-      <div className="invisible">
-            Test
+            )
+          )}
         </div>
         <div className="invisible">
             Test
@@ -40,8 +37,10 @@ function ChatWindowDisplay({messages}: ChatWindowProps) {
         <div className="invisible">
             Test
         </div>
-
-    </>
+        <div className="invisible">
+            Test
+        </div>
+    </div>
   );
 }
 
