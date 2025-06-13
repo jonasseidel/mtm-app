6bb8a8ef265105a5a8ef6313f0d763d11363762a
seideljonas <jonas.seidel1@rwth-aachen.de>
2025-06-13 10:58:19 +0200

    Added max width to messagesr

--- frontend/src/components/ChatWindowDisplay.tsx
+++ frontend/src/components/ChatWindowDisplay.tsx
@@ -9,19 +9,19 @@ function ChatWindowDisplay({messages}: ChatWindowProps) {
 
   return (
       <div className="container border rounded-4 min-vh-100 w-50 bg-light p-3">
-        <div className="vstack gap-3 mx-auto">
+        <div className="vstack gap-3 p-3 mx-auto">
           {messages.map(([message, source], index) =>
             source % 2 === 1 ? (
-              <div className="card rounded-4 align-self-end" key={`${index}-question`}>
+              <div className="card rounded-4 align-self-end" key={`${index}-question`} style={{ maxWidth: '85%' }}>
                 <div className="card-body">
-                  <h6 className="card-title text-primary">You</h6>
-                  <ReactMarkdown >{message}</ReactMarkdown>
+                  <h6 className="card-title text-primary text-break">You</h6>
+                  <ReactMarkdown>{message}</ReactMarkdown>
                   
                 </div>
               </div>
             ) : (
               
-              <div className="card rounded-4 align-self-start" key={`${index}-answer`}>
+              <div className="card rounded-4 align-self-start" key={`${index}-answer`} style={{ maxWidth: '85%' }}>
                 <div className="card-body">
                   <h6 className="card-title text-success">Bot</h6>
                   <ReactMarkdown >{message}</ReactMarkdown>
@@ -30,6 +30,12 @@ function ChatWindowDisplay({messages}: ChatWindowProps) {
               </div>
             )
           )}
+        </div>
+        <div className="invisible">
+            Test
+        </div>
+        <div className="invisible">
+            Test
         </div>
         <div className="invisible">
             Test
